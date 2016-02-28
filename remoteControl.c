18f5c27f423ec43c4fa9b6ac5db83f238bf1ca64
mherbert7 <marcusherbert7@gmail.com>
2016-02-28 14:56:19 +1000

    Fixed up some errors.

--- remoteControl.c
+++ remoteControl.c
@@ -1,17 +1,3 @@
-/*Learning TV Remote Kit Code
-    Copyright (C) 2016  Marcus Herbert
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 /*
  * remoteControl.c
  *
@@ -471,15 +457,11 @@ int main(void) {
 			PORTB &= ~(1<<PORTB1);
 			DDRB &= ~((1 << PINB1) | (1 << PINB2));
 			
-<<<<<<< HEAD
 			MCUSR &= ~(1 << WDRF); //Clear the flag
 			WDTCSR |= (1 << WDCE) | (1 << WDE);
 			WDTCSR = (1 << WDE); //System reset mode
 			
 			_delay_ms(17);
-=======
-			_delay_ms(1000);
->>>>>>> 317c924f7ead691e7407218fc5f77a8e61eec341
 		}
 		RESET_WATCHDOG();
 		#ifdef LAST_DEBUGGING
